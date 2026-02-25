5e61082fdb6a67d4a66bab221cd44a1bc88ad17b
Miguel <amiguelrubio27@gmail.com>
2026-02-25 23:44:02 +0100

    feat: Add use case to get a random character
    Implements a `GetRandomCharacter` use case to fetch a single random character from the repository.
    
    - Creates the `GetRandomCharacter` class, which generates a random character ID between 1 and 826 and calls `repository.getSingleCharacter`.
    - Registers the `GetRandomCharacter` use case in the `domainModule` for dependency injection.

--- composeApp/src/commonMain/kotlin/org/amrubio27/rickmortyapp/di/DomainModule.kt
+++ composeApp/src/commonMain/kotlin/org/amrubio27/rickmortyapp/di/DomainModule.kt
@@ -1,6 +1,9 @@
 package org.amrubio27.rickmortyapp.di
 
+import org.amrubio27.rickmortyapp.domain.GetRandomCharacter
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val domainModule = module {
+    factoryOf(::GetRandomCharacter)
 }
